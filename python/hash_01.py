318736fe4f3ab5a54b185847a863dd848ed81f39
leedahye2001 <leedahye2001@naver.com>
2022-10-11 14:16:46 +0900

    hash01

---
+++ python/hash_01.py
@@ -0,0 +1,10 @@
+def solution(nums):
+    answer = 0
+    n=len(nums)/2 # 최대로 가져갈 수 있는 개수
+    nums=list(set(nums)) # 중복제거
+    if len(nums)>=n:
+        answer=n
+    elif len(nums)<n:
+        answer=len(nums)
+    
+    return answer
