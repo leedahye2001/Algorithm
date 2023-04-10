9a8f5464c2be5d7faa6a654ef4951937be05a6e0
leedahye <leedahye2001@naver.com>
2023-04-10 13:43:49 +0900

    baekjoon(c++):9012

--- c++/9012.cpp
+++ c++/9012.cpp
@@ -4,39 +4,39 @@
 using namespace std;
 
 bool Ps(string str){
-    stack<char> VPS;
-    int length=str.length(); //괄호의 길이
+    stack<char>VPS;
+    int length=str.length();
+
     for(int i=0;i<length;i++){
-        char c=str[i];
+        char c = str[i];
+
         if(c=='('){
             VPS.push(c);
-        }
-        else{
+        } else {
             if(VPS.empty()){
                 return false;
-            }
-            else{
+            } else {
                 VPS.pop();
             }
         }
     }
+    // 스택이 비어있으면 YES
     return VPS.empty();
 }
 
 int main(){
-    
-    int t; //입력받는 개수
+    int t;
     cin>>t;
-    
+
     for(int i=0;i<t;i++){
-        string str; //괄호
-        cin>>str;
-        if(Ps(str)){
-           cout<<"YES"<<endl;
-        }
-        else{
-            cout<<"NO"<<endl;
-        }
+       string str;
+       cin>>str;
+
+       if(Ps(str)){
+        cout<<"YES"<<endl;
+       } else {
+        cout<<"NO"<<endl;
+       }
     }
 
     return 0;
