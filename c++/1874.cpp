8fea3b9f25876c2ac61f291e1abc3ae7eb04f7f6
leedahye <leedahye2001@naver.com>
2023-04-10 16:44:33 +0900

    baekjoon(c++):1874

--- c++/1874.cpp
+++ c++/1874.cpp
@@ -1,43 +1,46 @@
 #include<iostream>
 #include<stack>
 #include<string>
-
 using namespace std;
-int main() {
-	stack<int> s;
-	int n;
-	string ans;
 
-	cin >> n; 
-	int m = 0; 
-	while (n--) {
-		int x;
-		cin >> x; 
-		if (x > m) { 
-			while(x > m) {
-				s.push(++m);
-				ans += '+'; 
+int main(){
+	int n;
+	cin >> n;
+	string str;
+
+	stack<int> S;
+	int init=0;
+
+	while(n--){
+		int value;
+		cin>>value;
+
+		if(value>init){
+			while(value>init){
+				S.push(++init);
+				str+='+';
 			}
-			s.pop();
-			ans += '-'; //pop
-		}
-		else { 
-			bool found = false;
-			if (!s.empty()) {
-				if (x == s.top()) { 
-					found = true;
+			S.pop();
+			str+='-';
+		} else {
+			bool flag = false;
+			if(!S.empty()){
+				if(value==S.top()){
+					flag=true;
 				}
-				s.pop();
-				ans += '-';
+				S.pop();
+				str+='-';
 			}
-			if (!found) {
-				cout << "NO" << '\n';
-				return 0; 
+			if(!flag){
+				cout<<"NO"<<endl;
+				return 0;
 			}
 		}
 	}
-	for (auto x : ans) {
-		cout << x << '\n';
+
+	// 이걸 사용해야 배열 출력 됨
+	for (auto value : str) {
+		cout << value << '\n';
 	}
 	return 0;
 }
